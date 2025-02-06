6ceb10be18f980c74fd23ffd68299608296e97a4
Roxana <roxanadejescu@gmail.com>
2025-02-06 10:49:16 +0200

    Initial commit

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,28 @@
+// Allow the user to search for notes.
+// Pass the search term back to the parent component (App.js).
+import PropTypes from "prop-types";
+import './components.css';
+import search from './Images/search.png'
+
+function SearchBar({ onSearch }) {
+    const handleSearch = (e) => {
+      onSearch(e.target.value);
+    };
+  
+    return (
+      <div className="searchBar">
+        <img src={search} alt="Search" />
+        <input
+          type="text"
+          placeholder="Search notes..."
+          onChange={handleSearch}
+        />
+    </div>
+    );
+  }
+
+  SearchBar.propTypes = {
+    onSearch: PropTypes.func
+  };
+  
+  export default SearchBar;
